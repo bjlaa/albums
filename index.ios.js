0c1f2948ab4d895e869d9414e7acd8f774274bc8
Benjamin Arias <benjamin.j.arias@gmail.com>
2016-10-18 17:03:45 +0200

    feat: Created Header AlbumList and AlbumDetails components

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { View, AppRegistry } from 'react-native';
+
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-  <Text>Some Text</Text>
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />    
+  </View>
 );
 
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/AlbumList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import AlbumDetails from './AlbumDetails';
+
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      this.setState({ albums: data });
+    });
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <AlbumDetails key={album.title} album={album} />);
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );    
+  }
+}
+
+export default AlbumList;
